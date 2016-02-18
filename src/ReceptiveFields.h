23581d05ad223c074cfb2fd8da9063cab4812913
Martin <Baggex10@gmail.com>
2016-02-18 12:32:03 +0100

    Update ReceptiveFields.h

--- src/ReceptiveFields.h
+++ src/ReceptiveFields.h
@@ -14,9 +14,9 @@ class ReceptiveFields {
 		ReceptiveFields(const int lowerLimit, const int upperLimit, const int numberOfKernels);
 		virtual ~ReceptiveFields();
 	private:
-		const int lowerLimit;
-		const int upperLimit;
-		const int numberOfKernels;
+		const int LOWER_LIMIT;
+		const int UPPER_LIMIT;
+		const int NUMBER_OF_KERNELS;
 
 		virtual void createGaussianKernels();
 		virtual void applyDeltaRule(int iterations);
