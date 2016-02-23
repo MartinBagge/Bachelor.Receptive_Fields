18aed96c3d953b9bc5266e6074beffd78e5ef28e
skjoldd <sk.dyre@gmail.com>
2016-02-23 04:50:08 -0800

    l

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
-using namespace ReceptiveFields;
+using namespace RF::ReceptiveFields;
 
 int main(){
 	cout << "Test" << endl;
 
 	ReceptiveFields RF = new ReceptiveFields(45, 180, 20, 0.025, 0.2, 100, 1000);
-	cout << RF.toString();
-	cout << endl;
+	RF.toString();
 	return 0;
 }
