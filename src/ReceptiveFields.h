018e7803f42414c830ec06b015231586cda9e064
Martin <Baggex10@gmail.com>
2016-04-21 12:33:09 +0200

    Update ReceptiveFields.h
    Restructured and commented

--- src/ReceptiveFields.h
+++ src/ReceptiveFields.h
@@ -10,8 +10,9 @@
 
 #include <cmath>
 #include <string>
-#include <iostream>
 #include <vector>
+//TODO: delete when testing is done
+#include <iostream>
 #include <fstream>
 
 class ReceptiveFields {
@@ -20,42 +21,41 @@ class ReceptiveFields {
 		ReceptiveFields(const int lowerLimit, const int upperLimit, const int numberOfKernels, const double kernelWidth, const double learningRate, const int learningIterations, const int targetSize);
 
 		~ReceptiveFields();
-
+		//TODO: delete toString method when testing is done
 		virtual void toString();
-		virtual void generateTarget(double input);
 		virtual void createGaussianKernels();
 		virtual void generateAlfaPattern();
+		
+		//Method is used to give one point in the learning trajectory
+		virtual void generateTarget(double input);
 	private:
-		int targetcount;
 		const int lowerLimit;
 		const int upperLimit;
 		const int numberOfKernels;
 		const double kernelWidth;
-
+	
 		std::vector<double> gaussianKernels2d;
 		std::vector<double> kernelCenters1d;
 		std::vector<double> alfa1d;
 		std::vector<double> weights1d;
-		std::vector<double> targetPattern1d;
-		std::vector<double> transKernels2d;
 		std::vector<double> output1d;
 
 
-
+		//Util methods
 		virtual std::vector<double> linSpace(int start, int stop, int space);
 		virtual std::vector<double> zeros(int size);
 
 		//Learning properties
 
-
+		int targetcount;
 		const double learningRate;
 		const int learningIterations;
+		const int targetSize;
+		
+		std::vector<double> targetPattern1d;
 
 		virtual void applyDeltaRule();
 
-		//Learning target
-
-		const int targetSize;
 
 };
 #endif /* RECEPTIVEFIELDS_H_ */
