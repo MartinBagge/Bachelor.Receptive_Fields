f5efddfbf39440bc5ba7bf31cc7c539e1a8f609c
Martin <Baggex10@gmail.com>
2016-04-26 12:29:17 +0200

    Commented Parallelize.h

--- src/Parallelize.h
+++ src/Parallelize.h
@@ -7,11 +7,6 @@
 
 #ifndef PARALLELIZE_H_
 #define PARALLELIZE_H_
-#ifdef __CUDACC__
-#define CUDA_CALLABLE_MEMBER __host__ __device__
-#else
-#define CUDA_CALLABLE_MEMBER
-#endif
 
 #include <cuda_runtime_api.h>
 #include <cuda.h>
@@ -24,10 +19,16 @@ class Parallelize {
 	public:
 		Parallelize();
 		~Parallelize();
+		//GENERAL
+		//Call function to create "step" in kernel
 		std::vector<double> createKernels(std::vector<double> centers, double step, double width, int size);
+		//LEARNING
+		//function for simple supervised learning
 		std::vector<double> applyDeltaRule(double learningRate, std::vector<double> centers, std::vector<double> target, std::vector<double> weights, std::vector<double> kernels, int learningIterations);
 	private:
+		//prepare functions
 		void d_createKernels(double *centers, double step, double width, double *kernelsArr, int centerSize, int kernelSize);
+		//learning
 		void calcOutput(double *weights, double *kernels, double *output, int centerSize, int targetSize, int kernelSize);
 		void updateWeights(double learningRateValue, double *centers, double *target, double *output, double *weights, int centerSize, int targetSize);
 };
