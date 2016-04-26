288167d484bc86a2b94ff4981a245d01fd67d4ca
Martin <Baggex10@gmail.com>
2016-04-26 12:38:04 +0200

    commented ReceptiveFields.h

--- src/ReceptiveFields.h
+++ src/ReceptiveFields.h
@@ -19,21 +19,29 @@
 class ReceptiveFields {
 
 	public:
+		//FUNCTIONS
+		
+		//if use_gpu is set to true a nvidia gpu must be present in the system setup as this is required
 		ReceptiveFields(const int lowerLimit, const int upperLimit, const int numberOfKernels, const double kernelWidth, const double learningRate, const int learningIterations, const int targetSize,  bool use_gpu);
 
 		~ReceptiveFields();
-		//TODO: delete toString method when testing is done
+		//Used to create a value in each kernel
 		virtual void createStep(double step);
+		//Call when all steps are added to apply learning
 		virtual void applyDeltaRule();
+		//TODO: debugging
 		virtual void toString();
 		//Method is used to give one point in the learning trajectory
 		virtual void generateTarget(double input);
-		int targetcount;
-		const double learningRate;
-		const int learningIterations;
-		const int targetSize;
+		
+		
+		
+		//PROPERTIES
+		//GPU properties
 		Parallelize* para;
 		const bool use_gpu;
+		
+		//Generel properties
 		int kernelCreationCounter;
 		const int lowerLimit;
 		const int upperLimit;
@@ -41,12 +49,18 @@ class ReceptiveFields {
 		const double kernelWidth;
 		std::vector<double> gaussianKernels2d;
 		std::vector<double> kernelCenters1d;
-		std::vector<double> alfa1d;
-		std::vector<double> weights1d;
 		std::vector<double> output1d;
 		virtual std::vector<double> linSpace(double start, double stop, double space);
 		virtual std::vector<double> zeros(int size);
+		
+		//Properties for learning
+		std::vector<double> alfa1d;
+		std::vector<double> weights1d;
 		std::vector<double> targetPattern1d;
+		int targetcount;
+		const double learningRate;
+		const int learningIterations;
+		const int targetSize;
 
 };
 #endif /* RECEPTIVEFIELDS_H_ */
